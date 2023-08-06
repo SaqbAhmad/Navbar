b04c30bad08c961e2a5498e10aba5e1d052de955
Saquib Ahmad <syedsaqibahmad1@gmail.com>
2023-08-06 22:54:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log("hello guys");
+const toggle=document.querySelector(".nav-toggle");
+const links=document.querySelector(".links");
+toggle.addEventListener("click",function(){
+    // if(links.classList.contains("show-links")){
+    //     links.classList.remove("show-links");
+    // }
+    // else{
+    //     links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+})
